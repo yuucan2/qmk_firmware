00c03c51bc328c214227948626b86ec7b872592c
yuucan2 <113398662+yuucan2@users.noreply.github.com>
2025-07-14 06:23:18 +0900

    Update keymap.c

--- keyboards/handwired/onekey/keymaps/mykeymap/keymap.c
+++ keyboards/handwired/onekey/keymaps/mykeymap/keymap.c
@@ -21,6 +21,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t layer = biton32(state);
+    switch (layer) {
+        case 0:
+            rgblight_sethsv(HSV_RED);
+            break;
+        case 1:
+            rgblight_sethsv(HSV_BLUE);
+            break;
+        case 2:
+            rgblight_sethsv(HSV_GREEN);
+            break;
+        case 3:
+            rgblight_sethsv(HSV_WHITE);
+            break;
+    }
+    return state;
+}
+
+
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
   debug_enable=true;
